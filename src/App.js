64d9c3f3d0be9561cd4e80b92d22e6ca7ed0350a
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2017-03-28 16:06:26 +0100

    pagination working, index.html moved back into public

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
@@ -9,36 +10,40 @@ class App extends Component {
 
   constructor(props) {
     super(props);
+
     this.state = {
       pokemon: [],
       activePage: 0,
       limit: 50,
-      offSet: 0,
+      offset: 0,
       totalPages: 0
     };
 
     this.loadPokemon = this.loadPokemon.bind(this);
+    this.handlePaginationSelect = this.handlePaginationSelect.bind(this);
   }
 
   loadPokemon(url) {
     fetch(url)
-    .then(response => {
-      return response.json();
-    }).then(json => {
-      let pages = Math.round(json.count / this.state.limit);
+      .then(response => {
+        return response.json();
+      }).then(json => {
+        console.log(json);
+        let pages = Math.round(json.count / this.state.limit);
 
-      this.setState({
-        pokemon: json.result,
-        totalPages: pages,
-        count: json.count
-      });
-    }).catch(err => {
-      console.log(err)
-    })
+        this.setState({
+          pokemon: json.results,
+          totalPages: pages,
+          count: json.count
+        });
+        console.log(this.state)
+      }).catch(err => {
+        console.log(err)
+      })
   }
 
   componentWillMount() {
-    this.loadPokemon(`${this.props.baseUrl}/pokemon/`);
+    this.loadPokemon(`${this.props.baseUrl}/pokemon/?limit=${this.state.limit}&offset=${this.state.offset}`);
   }
 
   handlePaginationSelect(selectedPage) {
@@ -54,16 +59,19 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
+
         <Col sm={8} md={10} smOffset={2} mdOffset={1} >
-        <PokeList listOfPokemon={this.state.pokemon}/></Col>
-          <Col sm={12} >
-                <Pagination
-                  bsSize="small"
-                  items={this.state.totalPages}
-                  activePage={this.state.activePage}
-                  onSelect={this.handlePaginationSelect}
-                />
-          </Col>
+          <PokeList listOfPokemon={this.state.pokemon} />
+        </Col>
+
+        <Col sm={12} >
+          <Pagination
+            bsSize="small"
+            items={this.state.totalPages}
+            activePage={this.state.activePage}
+            onSelect={this.handlePaginationSelect}
+          />
+        </Col>
       </div>
     );
   }
