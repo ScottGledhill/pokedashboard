108481916ddbf97b1fedebf15c559d537639144d
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2017-03-28 14:48:56 +0100

    base url defined

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'whatwg-fetch';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
   render() {
     return (
       <div className="App">
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+let baseUrl = 'http://pokeapi.co/api/v2/'
+
 ReactDOM.render(
   <App />,
   document.getElementById('root')
