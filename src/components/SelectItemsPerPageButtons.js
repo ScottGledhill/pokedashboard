d2da94a5e6098fd81f590f1446b70fa3febad993
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2017-03-28 16:39:33 +0100

    component completed and exported

--- src/components/SelectItemsPerPageButtons.js
+++ src/components/SelectItemsPerPageButtons.js
@@ -1,4 +1,16 @@
 import React from 'react';
 import {Button, Col} from 'react-bootstrap/lib/';
 
-const SelectItemsPerPageButtons = ({options, onOptionSelected, selectedValue})
+const SelectItemsPerPageButtons = ({options, onOptionSelected, selectedValue, allValue}) => {
+
+  return (
+    <Col sm={12} >
+    {options.map(option) =>
+      <Button key={option} onClick={onOptionSelected} bsStyle={selectedValue === option ? 'primary' : 'default'}>{option}</Button>
+    })}
+    {allValue ? <Button key={allValue} onClick={onOptionSelected} bsStyle={selectedValue === 'primary' : 'default' }> </Button> : false}
+    </Col>
+  )
+}
+
+export default SelectItemsPerPageButtons;
